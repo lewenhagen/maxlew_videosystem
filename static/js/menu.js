5968b15fb4596b44da7db0f5c332f405c45a20f3
lewenhagen <lewenhagen@live.com>
2020-03-24 20:32:32 +0100

    cleaned menu js

--- static/js/menu.js
+++ static/js/menu.js
@@ -3,20 +3,17 @@
 
     document.addEventListener("keydown", function(event) {
         let key = event.key;
-        // console.log(event);
+        console.log(event);
         switch(key) {
             case "1":
             case "2":
             case "3":
             case "4":
             case "5":
-                window.location.replace(`/${key}`);
-                break;
             case "6":
-                console.log("yay");
-                break;
+            case "7":
             case "8":
-
+                window.location.replace(`/${key}`);
                 break;
             case "9":
                 window.location.replace(`/splashscreen`);
